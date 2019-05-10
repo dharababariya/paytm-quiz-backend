8a4e5e65bfdd9ed4a82352b4f7bf7cc80fae1c92
Dhara <dharababariya34@gmail.com>
2019-05-10 12:09:18 +0530

    setup app

---
+++ db/knex.js
@@ -0,0 +1,17 @@
+const knex = require('knex')({
+
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    ssl : true,
+    debug: false
+  
+
+  })
+
+
+//expose knex connection object;
+module.exports = knex;
